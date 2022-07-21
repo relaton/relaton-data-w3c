f439c7ffd915ee1ea6c008431ec50d469c9b30c6
Alex Bobrikovich <abobrikovich@gmail.com>
2022-07-21 23:29:39 +0200

    Move to unified workflow (#9)
    * Move to unified workflow

---
+++ crawler.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'fileutils'
+require 'relaton_w3c'
+
+FileUtils.rm_rf('data')
+FileUtils.rm Dir.glob('index-w3c.*')
+
+system("git clone https://github.com/relaton/w3c-tr-archive w3c-tr-archive")
+
+RelatonW3c::DataFetcher.fetch 'w3c-tr-archive'
+RelatonW3c::DataFetcher.fetch 'w3c-rdf'
+
+system("zip index-w3c.zip index-w3c.yaml")
+system("git add index-w3c.yaml index-w3c.zip")
