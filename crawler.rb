96f7b817e83eef264cc58a8a721e015e8949726c
Alexande B <abobrikovich@gmail.com>
2022-07-21 19:12:49 +0200

    Update crawler to use relaton-w3c gem directly

--- crawler.rb
+++ crawler.rb
@@ -1,12 +1,16 @@
 # frozen_string_literal: true
 
+require 'bundler/setup'
 require 'fileutils'
+require 'relaton-w3c'
 
 FileUtils.rm_rf('data')
 FileUtils.rm Dir.glob('index-w3c.*')
 
 system("git clone https://github.com/relaton/w3c-tr-archive w3c-tr-archive")
-system("relaton fetch-data w3c-tr-archive")
-system("relaton fetch-data w3c-rdf")
+
+RelatonW3c::DataFetcher.fetch 'w3c-tr-archive'
+RelatonW3c::DataFetcher.fetch 'w3c-rdf'
+
 system("zip index-w3c.zip index-w3c.yaml")
 system("git add index-w3c.yaml index-w3c.zip")
