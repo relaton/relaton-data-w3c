179bb82f057df0476a134af78347dbd85f469e9a
Alexande B <abobrikovich@gmail.com>
2022-07-21 17:18:52 +0200

    Update crawler

--- crawler.rb
+++ crawler.rb
@@ -3,6 +3,10 @@
 require 'fileutils'
 
 FileUtils.rm_rf('data')
+FileUtils.rm Dir.glob('index-w3c.*')
 
+system("git clone https://github.com/relaton/w3c-tr-archive w3c-tr-archive")
+system("relaton fetch-data w3c-tr-archive")
 system("relaton fetch-data w3c-rdf")
+system("zip index-w3c.zip index-w3c.yaml")
 system("git add index-w3c.yaml index-w3c.zip")
