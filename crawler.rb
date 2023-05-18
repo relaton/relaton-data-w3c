6dd1d2e8c6f0e4934f9ca7bbece56af544356872
Andrei Kislichenko <android.2net@gmail.com>
2023-05-17 23:38:02 -0400

    add new index

--- crawler.rb
+++ crawler.rb
@@ -4,7 +4,7 @@ require 'fileutils'
 require 'relaton_w3c'
 
 FileUtils.rm_rf('data')
-FileUtils.rm Dir.glob('index-w3c.*')
+FileUtils.rm Dir.glob('index*')
 
 system("git clone https://github.com/relaton/w3c-tr-archive w3c-tr-archive")
 
@@ -12,4 +12,5 @@ RelatonW3c::DataFetcher.fetch 'w3c-tr-archive'
 RelatonW3c::DataFetcher.fetch 'w3c-rdf'
 
 system("zip index-w3c.zip index-w3c.yaml")
+system("zip index1.zip index1.yaml")
 system("git add index-w3c.yaml index-w3c.zip")
