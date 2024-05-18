82a4e1248be40871c21254426c75761cf1c02f9c
Alex Babrykovich <abobrikovich@gmail.com>
2024-05-19 00:23:10 +0200

    feat: do shallow clone in crawler.rb

--- crawler.rb
+++ crawler.rb
@@ -6,7 +6,7 @@ require 'relaton_w3c'
 FileUtils.rm_rf('data')
 FileUtils.rm Dir.glob('index*')
 
-system("git clone https://github.com/relaton/w3c-tr-archive w3c-tr-archive")
+system("git clone --depth=1 https://github.com/relaton/w3c-tr-archive w3c-tr-archive")
 
 RelatonW3c::DataFetcher.fetch 'w3c-tr-archive'
 RelatonW3c::DataFetcher.fetch 'w3c-rdf'
