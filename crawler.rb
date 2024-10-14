1e3317338a8f3faae43eabdb080f3e81494290d3
Andrei Kislichenko <android.2net@gmail.com>
2024-10-14 10:41:47 -0400

    use RDF archive relaton/relaton-w3c#38

--- crawler.rb
+++ crawler.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
-require 'fileutils'
+# require 'fileutils'
 require 'relaton_w3c'
 
 FileUtils.rm_rf('data')
 FileUtils.rm Dir.glob('index*')
 
-system("git clone https://github.com/relaton/w3c-tr-archive w3c-tr-archive")
+# system("git clone https://github.com/relaton/w3c-tr-archive w3c-tr-archive")
 
-RelatonW3c::DataFetcher.fetch 'w3c-tr-archive'
-RelatonW3c::DataFetcher.fetch 'w3c-rdf'
+RelatonW3c::DataFetcher.fetch # output: 'dir' # 'w3c-tr-archive'
+
+system("git add archive.rdf")
