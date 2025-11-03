07eaa0fdd88b1c41b946e6150aef7a9921ef18d0
Ronald Tse <ronald.tse@ribose.com>
2025-11-03 23:31:39 +0800

    feat: update crawler (#13)

--- crawler.rb
+++ crawler.rb
@@ -6,8 +6,4 @@ require 'relaton_w3c'
 FileUtils.rm_rf('data')
 FileUtils.rm Dir.glob('index*')
 
-# system("git clone https://github.com/relaton/w3c-tr-archive w3c-tr-archive")
-
-RelatonW3c::DataFetcher.fetch # output: 'dir' # 'w3c-tr-archive'
-
-system("git add archive.rdf")
+RelatonW3c::DataFetcher.fetch
